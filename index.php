1c6701d554ec52531768aa995cfd45322a08d283
tomoexx <148716817+tomoexx@users.noreply.github.com>
2026-02-10 10:40:05 +0700

    force git pull requrest

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 -->
 
+// force git pull requrest
+
 <?php
 
 // ทำการ import โค้ดเชื่อมต่อฐานข้อมูลจากไฟล์ con_db.php เพื่อให้โค้ดในไฟล์นี้ทำงานร่วมกับฐานข้อมูลได้
